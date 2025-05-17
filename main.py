c34a5d3cc35eb9dffaf992953a23361f33cf5ec6
Rob Hay <robhay@outlook.com>
2025-05-17 17:11:16 +0100

    Fix import path for models

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ import pandas as pd
 import joblib
 import xgboost as xgb
 import os
-from .models import MoistureLog, IrrigationLog, WeatherHistory, PredictionMeta
-
-
-from models import MoistureLog, IrrigationLog
+from models import MoistureLog, IrrigationLog, WeatherHistory, PredictionMeta
 from database import Base, engine, SessionLocal
 
 app = FastAPI()
